c57fda7f1a472a5f226a63d9f9ac8a9f0f5092f5
Viktoria Krolikowski <viktoria.krolikowski.emnify@gmail.com>
2017-10-09 13:17:05 +0200

    added deep registration to avoid missing fields

--- pingpong/src/main/java/akkakryotest/AkkaKryoInitializer.java
+++ pingpong/src/main/java/akkakryotest/AkkaKryoInitializer.java
@@ -5,15 +5,31 @@ import com.esotericsoftware.kryo.Kryo;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 
+import java.lang.reflect.Field;
 import java.util.List;
 
 public class AkkaKryoInitializer {
+    private int hash(String s) {
+        return MurmurHash.stringHash(s) >>> 1;
+    }
+
+    private void registerClassDeep(Kryo kryo, Class<?> type) {
+        try {
+            kryo.getRegistration(type);
+        } catch (IllegalArgumentException iae) {
+            kryo.register(type, hash(type.getName()));
+        }
+        for (Field f : type.getDeclaredFields()) {
+            registerClassDeep(kryo, f.getType());
+        }
+    }
+
     public void customize(Kryo kryo) throws ClassNotFoundException {
         Config conf = ConfigFactory.load();
         List<String> classes = conf.getStringList("akka.actor.kryo.classes-unordered");
         for (String className : classes) {
             try {
-                kryo.register(Class.forName(className), MurmurHash.stringHash(className) >>> 1);
+                registerClassDeep(kryo, Class.forName(className));
             } catch (Throwable t) {
                 t.printStackTrace();
             }
